8ce4003d2ed3cb212815c0f03611a8a490bbbccc
赵一欢 <happy@sdblo.com>
2018-06-20 10:58:02 +0800

    t

---
+++ MyStoryboardStudy/day1-my1stStoryboard/day1-my1stStoryboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  day1-my1stStoryboard
+//
+//  Created by 赵一欢 on 2018/1/23.
+//  Copyright © 2018年 zhaoyihuan. All rights reserved.
+//
+
+
+//
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
